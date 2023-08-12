e9683b1b23d802008311d57487e9cb6b2679a6fa
HaraTakae <140220718+HaraTakae@users.noreply.github.com>
2023-08-12 08:47:10 +0000

    Add

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,23 +4,25 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/top'
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to book_path(@book.id)
   end
 
   def index
     @books = Book.all
+    @book= Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
   end
 
-  private
-  def book_params
-    params.require(:book).parmit(:title, :body)
-  end
+ private
+ def book_params
+  params.require(:book).permit(:title, :body)
+ end
 end
